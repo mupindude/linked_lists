6dfe7543f85ef110d15b5ebae8e3f94d14628067
mupindude <mupindudevine@gmail.com>
2026-06-25 13:48:03 +0400

    Refactor ft_list_remove_if with t_list structure
    Refactor ft_list_remove_if to use t_list structure and fix node removal logic.

--- ft_list_remove_if.c
+++ ft_list_remove_if.c
@@ -1,29 +1,45 @@
-#include "prac_list.h"
+#include <stdlib.h>
 
-void ft_list_remove_if(Node **begin_list, void *data_ref, int (*cmp)())
+typedef struct s_list
 {
-    Node *temp;
-    Node *current;
+    struct s_list *next;
+    void          *data;
+} t_list;
+
+void ft_list_remove_if(
+    t_list **begin_list,
+    void *data_ref,
+    int (*cmp)(void *, void *)
+)
+{
+    t_list *current;
+    t_list *prev;
+    t_list *tmp;
 
     if (!begin_list || !*begin_list)
         return;
-    while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
-    {
-        temp = *begin_list;
-        begin_list = (*begin_list)->next;
-        free(temp);
-    }
 
     current = *begin_list;
-    while (current  && current->next)
+    prev = NULL;
+
+    while (current)
     {
-        if (cmp(current->next->data, data_ref) == 0)
+        if (cmp(current->data, data_ref) == 0)
         {
-            temp = current->next;
-            current->next = temp->next;
-            free(temp);
-        }
-        else 
+            tmp = current;
+
+            if (prev == NULL)
+                *begin_list = current->next;
+            else
+                prev->next = current->next;
+
             current = current->next;
+            free(tmp);
+        }
+        else
+        {
+            prev = current;
+            current = current->next;
+        }
     }
 }
